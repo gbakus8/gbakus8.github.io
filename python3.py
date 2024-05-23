3b42af9cc3ffaeca1b51d9c2cb779a420aef0b24
premy <p.remy@sap.com>
2024-05-23 17:49:25 +0200

    python

--- python3.py
+++ python3.py
@@ -1,149 +1,170 @@
-#!/usr/local/bin/python3
-
 import os
-import http.server
 import socketserver
 
-PORT = 8000
-
-# Chemin du répertoire contenant les images
-img_dir = 'img'
-
-# Template HTML pour la galerie principale
-gallery_template = """
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gallery</title>
-    <style>
-        .thumbnail {
-            max-width: 200px;
-            display: inline-block;
-            margin: 5px;
-            cursor: pointer;
-        }
-
-        #myModal {
-            display: none;
-            position: fixed;
-            z-index: 1;
-            padding-top: 100px;
-            left: 0;
-            top: 0;
-            width: 100%;
-            height: 100%;
-            overflow: auto;
-            background-color: rgb(0,0,0);
-            background-color: rgba(0,0,0,0.9);
-        }
-
-        .modal-content {
-            margin: auto;
-            display: block;
-            width: 80%;
-            max-width: 700px;
-        }
-
-        #caption {
-            margin: auto;
-            display: block;
-            width: 80%;
-            max-width: 700px;
-            text-align: center;
-            color: #ccc;
-            padding: 10px 0;
-            height: 150px;
-        }
-
-        .close {
-            position: absolute;
-            top: 15px;
-            right: 35px;
-            color: #f1f1f1;
-            font-size: 40px;
-            font-weight: bold;
-            transition: 0.3s;
-        }
-
-        .close:hover,
-        .close:focus {
-            color: #bbb;
-            text-decoration: none;
-            cursor: pointer;
-        }
-    </style>
-</head>
-<body>
-    <h1>Gallery</h1>
-    <div id="thumbnails">
-        <!-- Thumbnails will be inserted here -->
-    </div>
-
-    <!-- The Modal -->
-    <div id="myModal" class="modal">
-        <span class="close">&times;</span>
-        <img class="modal-content" id="modalImg">
-        <div id="caption"></div>
-    </div>
-
-    <script>
-        // JavaScript to handle thumbnail clicks
-        var thumbnails = document.querySelectorAll('.thumbnail');
-        thumbnails.forEach(function(thumbnail) {
-            thumbnail.addEventListener('click', function() {
-                var modal = document.getElementById("myModal");
-                var modalImg = document.getElementById("modalImg");
-                var captionText = document.getElementById("caption");
-
-                modal.style.display = "block";
-                modalImg.src = this.src;
-                captionText.innerHTML = this.alt;
-            });
-        });
-
-        // Close the modal
-        var closeBtn = document.getElementsByClassName("close")[0];
-        closeBtn.onclick = function() {
-            var modal = document.getElementById("myModal");
-            modal.style.display = "none";
-        }
-    </script>
-</body>
-</html>
-"""
-
-# Handler pour servir les fichiers et gérer les routes
-class GalleryHandler(http.server.SimpleHTTPRequestHandler):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, directory='.', **kwargs)
-
-    def list_image_files(self):
-        image_files = []
-        for filename in os.listdir(img_dir):
-            if filename.endswith('.jpg') or filename.endswith('.jpeg') or filename.endswith('.png'):
-                image_files.append(filename)
-        return image_files
-
-    def list_image_thumbnails(self):
-        thumbnails = ''
-        image_files = self.list_image_files()
-        for filename in image_files:
-            thumbnails += f'<img class="thumbnail" src="/img/{filename}" alt="{filename}">'
-        return thumbnails
-
-    def do_GET(self):
-        if self.path == '/':
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            self.wfile.write(gallery_template.encode('utf-8'))
-            self.wfile.write(self.list_image_thumbnails().encode('utf-8'))
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        request = self.request.recv(1024).decode()
+        if request.startswith("GET"):
+            if request.split(" ")[1] == "/":
+                self.send_index_page()
+            elif request.split(" ")[1].endswith("jpg"):
+                self.send_image_file(request.split(" ")[1].split("/")[1])
+            elif request.split(" ")[1] == "/styles.css":
+                self.send_css_file()
+            elif request.split(" ")[1] == "/index.js":
+                self.send_js_file()
+            else:
+                self.send_404_error()
+        elif request.startswith("POST"):
+            pass
         else:
-            super().do_GET()
+            self.send_400_error()
 
-# Lance le serveur
-with socketserver.TCPServer(("", PORT), GalleryHandler) as httpd:
-    print("Serveur démarré sur le port", PORT)
-    httpd.serve_forever()
+    def send_400_error(self):
+        self.request.sendall(b"HTTP/1.1 400 Bad Request\r\n\r\n")
+
+    def send_404_error(self):
+        self.request.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+
+    def send_index_page(self):
+        html_file = open("index.html", "rb").read()
+        self.request.sendall(b"HTTP/1.1 200 OK\r\n")
+        self.request.sendall(b"Content-Type: text/html\r\n")
+        self.request.sendall(b"\r\n")
+        self.request.sendall(html_file)
+
+    def send_image_file(self, filename):
+        image_file = open(f"img/{filename}", "rb").read()
+        self.request.sendall(b"HTTP/1.1 200 OK\r\n")
+        self.request.sendall(b"Content-Type: image/jpeg\r\n")
+        self.request.sendall(b"\r\n")
+        self.request.sendall(image_file)
+
+    def send_css_file(self):
+        css_file = open("styles.css", "rb").read()
+        self.request.sendall(b"HTTP/1.1 200 OK\r\n")
+        self.request.sendall(b"Content-Type: text/css\r\n")
+        self.request.sendall(b"\r\n")
+        self.request.sendall(css_file)
+
+    def send_js_file(self):
+        js_file = open("index.js", "rb").read()
+        self.request.sendall(b"HTTP/1.1 200 OK\r\n")
+        self.request.sendall(b"Content-Type: text/javascript\r\n")
+        self.request.sendall(b"\r\n")
+        self.request.sendall(js_file)
+
+if __name__ == "__main__":
+    with socketserver.TCPServer(("", 8080), MyTCPHandler) as server:
+        server.serve_forever()
+```
+
+**styles.css**
+
+```css
+body {
+  font-family: Arial, sans-serif;
+  text-align: center;
+}
+
+#banner {
+  width: 100%;
+  height: 100px;
+  background-image: url("catsdogsloversclubbanner.png");
+  background-size: cover;
+}
+
+#gallery {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: repeat(4, 1fr);
+}
+
+#gallery img {
+  max-width: 200px;
+  max-height: 200px;
+}
+
+#photo {
+  width: 100%;
+  max-width: 1800px;
+  max-height: 1800px;
+}
+
+.button {
+  padding: 10px 20px;
+  background-color: #333;
+  color: #fff;
+  text-decoration: none;
+  border: none;
+}
+```
+
+**index.html**
+
+```html
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Cats & Dogs Lovers Club</title>
+    <link rel="stylesheet" href="styles.css" />
+  </head>
+  <body>
+    <div id="banner"></div>
+    <h1>Cats & Dogs Lovers Club</h1>
+    <div id="gallery">
+      {% for image in os.listdir("img") %}
+      <a href="{{ image }}">
+        <img src="{{ image }}" />
+      </a>
+      {% endfor %}
+    </div>
+    <script src="index.js"></script>
+  </body>
+</html>
+```
+
+**index.js**
+
+```javascript
+const galleryImages = document.querySelectorAll("#gallery img");
+const photo = document.getElementById("photo");
+const buttons = document.querySelectorAll(".button");
+
+let currentIndex = 0;
+
+function showImage(index) {
+  photo.src = galleryImages[index].src;
+  currentIndex = index;
+  updateButtons();
+}
+
+function nextImage() {
+  currentIndex += 1;
+  if (currentIndex >= galleryImages.length) {
+    currentIndex = 0;
+  }
+  showImage(currentIndex);
+}
+
+function previousImage() {
+  currentIndex -= 1;
+  if (currentIndex < 0) {
+    currentIndex = galleryImages.length - 1;
+  }
+  showImage(currentIndex);
+}
+
+function updateButtons() {
+  buttons[0].disabled = currentIndex === 0;
+  buttons[1].disabled = currentIndex === galleryImages.length - 1;
+}
+
+galleryImages.forEach((image, index) => {
+  image.addEventListener("click", () => {
+    showImage(index);
+  });
+});
+
+buttons[0].addEventListener("click", previousImage);
+buttons[1].addEventListener("click", nextImage);
